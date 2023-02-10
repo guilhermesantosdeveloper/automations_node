6e3407574d9c7d0e5d92d92a5e7a998f889488d8
Guilherme Santos <guilhermesantos.developer@gmail.com>
2023-02-10 09:46:00 -0300

    aula 2

---
+++ 2_classe_puppeteer/index.js
@@ -0,0 +1,13 @@
+// fazendo o pupperter enetra em um site e tirar uma screenshot
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 300 // parametro para deixar lento
+  })
+  const page = await browser.newPage();
+  await page.goto('https://stackoverflow.com/questions/68507849/eslint-uninstall');
+  await page.screenshot({ path: 'google.png' });
+  await browser.close();
+})()
